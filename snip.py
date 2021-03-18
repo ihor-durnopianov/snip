24bd032be834e1633b54c9869f7167f23fabd5a4
Ihor Durnopianov <ihor.durnopianov@outlook.com>
2021-03-18 10:05:20 +0200

    Implement add

--- snip.py
+++ snip.py
@@ -7,6 +7,15 @@ Usage:
 
 
 import argparse
+import tempfile
+import pathlib
+import random
+import string
+import json
+import os
+
+
+SNIPPETS_PATH = pathlib.Path("/home/ihor/.config/Code/User/snippets")
 
 
 def main():
@@ -46,7 +55,60 @@ def _assemble_commands():
 
 
 def _add(lang, prefix, **kwargs):
-    pass
+    snippet = _make_snippet(lang, prefix)
+    if True:
+        reviewed = _review_snippet(snippet)
+    dest = SNIPPETS_PATH / f"{lang}.json"
+    if not dest.exists():
+        if input(f"{dest} does not exist.  Create? (y/n) ") != "y":
+            return
+        with open(dest, "w") as file:
+            json.dump({}, file)
+    _save_snippet(reviewed, dest)
+
+
+def _save_snippet(snippet, dest):
+    # Expects dest to exist
+    with open(dest) as file:
+        snippets = json.load(file)
+    with open(dest, "w") as file:
+        json.dump(snippets | snippet, file, indent=4)
+
+
+def _make_snippet(lang, prefix):
+    return {
+        _make_random_name(): {
+            "prefix": prefix,
+            "body": [line for line in _request_body().splitlines()],
+            "description": ""
+        }
+    }
+
+
+def _request_body():
+    _, name = tempfile.mkstemp()
+    os.system(f"nano {name}")
+    with open(name) as file:
+        body = file.read()
+    os.unlink(name)
+    return body
+
+
+def _review_snippet(snippet):
+    _, name = tempfile.mkstemp()
+    with open(name, "w") as file:
+        json.dump(snippet, file, indent=4)
+    os.system(f"nano {name}")
+    with open(name) as file:
+        snippet = json.load(file)
+    os.unlink(name)
+    return snippet
+
+
+def _make_random_name():
+    return "".join(
+        random.choices(string.ascii_lowercase + string.digits, k=16)
+    )
 
 
 if __name__ == "__main__":
