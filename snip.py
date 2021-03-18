4aa89d0eb6c102fa0f1b7ca7620dff0a7914e31c
Ihor Durnopianov <ihor.durnopianov@outlook.com>
2021-03-18 08:43:50 +0200

    Init

---
+++ snip.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""A simple tool to help with snippets."""
+
+
+def main():
+    """Entry point of the application."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
